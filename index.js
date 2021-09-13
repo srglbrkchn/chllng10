e3d4157ebceee9a1667feb07e4c18352bc06ee38
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-09-13 22:19:39 +0200

    modify menu bar.

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 let status = "hidden";
+let open = 0;
 
 
 function buttonActive() {
   document.querySelector(".hamburger").style.color = "rgba(250, 250, 250, .7)";
   document.querySelector(".menu-buttons").style.visibility = "visible";
   status = "appear";
+  open = 1;
 }
 
 function buttonInactive() {
   document.querySelector(".hamburger").style.color = "#fff";
   document.querySelector(".menu-buttons").style.visibility = "hidden";
   status = "hidden";
+  open = 0;
 }
 
 document.querySelector(".hamburger").addEventListener("click", function() {
-  if(status === "hidden") {
+  if (status === "hidden") {
     buttonActive();
   } else {
     buttonInactive();
@@ -23,9 +26,14 @@ document.querySelector(".hamburger").addEventListener("click", function() {
 
 function buttonAppear() {
   let windowWidth = window.innerWidth;
-  if(windowWidth > 1120) {
+  if (windowWidth > 1120) {
     document.querySelector(".menu-buttons").style.visibility = "visible";
-  }else {
-    document.querySelector(".menu-buttons").style.visibility = "hidden";
+  } else {
+
+    if (open === 1) {
+      buttonActive();
+    } else {
+      buttonInactive();
+    }
   }
 }
